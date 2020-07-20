f254f7c0e5f8c1d4e95118d94b6e626a6ff0f57d
Miller <macorreag@unal.edu.co>
2020-07-19 22:59:47 -0500

    rename app

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('angular-whishlist app is running!');
+    expect(page.getTitleText()).toEqual('angular-Routebook app is running!');
   });
 
   afterEach(async () => {
